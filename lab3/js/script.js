d92bbefe1413dc872deff05a05d8b6c202d5c5e4
Tianze Yin <deyin12345@gmail.com>
2025-01-31 01:26:48 -0800

    change displayOrderSummary() to keep two decimal places, and recode the calculation of the toppingsPrice

--- lab3/js/script.js
+++ lab3/js/script.js
@@ -1,74 +1,36 @@
-let user;
-do {
-  user = prompt("What is your name?");
-} while (isNaN(user) == false || user.length === 1);
-console.log(typeof user); // string
-
-let students = [
-  {
-    name: "Cristian",
-    age: 30,
-    location: "Vancouver",
-  },
-  {
-    name: "James",
-    age: 40,
-    location: "Toronto",
-  },
-  {
-    name: "Garry",
-    age: 20,
-    location: "Vancouver",
-  },
-];
-
-function findStudentsInVancouver() {
-  let vancouverStudents = [];
-  for (let i = 0; i < students.length; i++) {
-    if (students[i].location === "Vancouver") {
-      console.log(`${students[i].name} lives in ${students[i].location}`);
-      vancouverStudents.push(students[i]);
-      // console.log(students[i].name);
-    }
-  }
-  return vancouverStudents;
-}
-
-const renturnedArray = findStudentsInVancouver(); // Cristian, Garry
-console.log(renturnedArray);
-
-
 const prices = { original: 2.5, mango: 3.0, strawberry: 3.5, small: 1.0, 
   medium: 1.5, large: 2.0, boba: 0.5, jelly: 0.75, pudding: 1.0 };
  
-
 
 function displayOrderSummary(order) {
   // example order object: 
   // {flavor: "mango", size: "medium", toppings: ["boba", "jelly"], finalPrice: 5.63 }
   // const toppingsList = order.toppings.join(", ");
-  console.log(`You have ordered a ${order.size} ${order.flavor} boba with these toppings: ${order.toppings.join(" ")}\nTotal price: $${order.finalPrice}`);
+  console.log(`You have ordered a ${order.size} ${order.flavor} boba with these toppings: ${
+    order.toppings.join(" ")}\nTotal price: $${order.finalPrice.toFixed(2)}`);
 }
 
-let order = {flavor: "mango", size: "medium", toppings: ["boba", "jelly"], finalPrice: 5.63 }
-displayOrderSummary(order);
 
 
 function placeOrder(flavor, size, toppings) {
-  // const prices = { original: 2.5, mango: 3.0, strawberry: 3.5, small: 1.0, medium: 1.5, large: 2.0, boba: 0.5, jelly: 0.75, pudding: 1.0 };
-
   let flavorPrice = prices[flavor];
   let sizePrice = prices[size];
-  let toppingsPrice = toppings.reduce((total, topping) => total + prices[topping], 0);
-
+  let toppingsPrice = 0;
+  for (i = 0; i < toppings.length; i++) {
+    toppingsPrice = toppingsPrice+ prices[toppings[i]];
+  }
+  
   let finalPrice = sizePrice * (flavorPrice + toppingsPrice);
-
 
   let order = {flavor: flavor, size: size, toppings: toppings, finalPrice: finalPrice}
   displayOrderSummary(order)
-  }
+}
 
-let flavor = "original";
+// Test cases
+let order = {flavor: "original", size: "medium", toppings: ["boba", "jelly"], finalPrice: 5.63 }
+displayOrderSummary(order);
+  
+let flavor = "mango";
 let size = "medium";
 let toppings = ["boba", "jelly"];
 placeOrder(flavor, size, toppings);
