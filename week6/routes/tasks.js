0cbc5fda36e67bdfd86572608fb2101b3f92cd4e
Tianze Yin <deyin12345@gmail.com>
2025-02-20 12:48:04 -0800

    Refactor task routes to use async/await for API calls and enhance task detail view with completion status

--- week6/routes/tasks.js
+++ week6/routes/tasks.js
@@ -3,25 +3,52 @@ const router = express.Router();
 const axios = require("axios");
 
 // this is the handler for the /tasks route
-router.get("/", function (req, res) {
-  const promise = axios.get("https://jsonplaceholder.typicode.com/todos");
-  promise.then((response) => {
+router.get("/", async (req, res) => {
+  try {
+    const response = await axios.get(
+      "https://jsonplaceholder.typicode.com/todos"
+
+    );
     res.json(response.data);
-    console.log(response.data);
-  });
+    
+
+  } catch (error) {
+    console.log(error.message);
+  }
+  // console.log(response);
+  /*
+  promise
+    .then((response) => {
+      res.json(response.data);
+      console.log(response.data);
+    })
+    .catch((error) => {
+      console.log(errors.status);
+    });
   console.log(promise);
+  */
 
   // res.send("<h1>List of All Tasks QQQpQ</h1>");
   // console.log(req.params);
   // console.log(req.query);
 });
 
-router.get("/:taskId", function (req, res) {
+router.get("/:taskId", async (req, res) => {
+    try {
+  const response = await axios.get(`https://jsonplaceholder.typicode.com/todos/${req.params.taskId}`);
+    console.log(response.data);
+    res.render("task", { id: req.params.taskId,tilte: response.data.title, completed: response.data.completed });
+
+  }
+  catch (error) {
+    console.log(error.message);
+  }
+
   // res.send('<h1>List of All Tasks QQQQQQ</h1>');
-  console.log("I am in the tasks/:taskId route");
-  console.log(req.params.taskId);
+  // console.log("I am in the tasks/:taskId route");
+  // console.log(req.params.taskId);
   // res.send(`<p>you are viewing task ${req.params.taskId}<p>`);
-  res.render("task", { id: req.params.taskId });
+ 
 });
 
 module.exports = router;
