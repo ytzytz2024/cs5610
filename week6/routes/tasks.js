ab445355372a9c5ef3a11add8ad60180e9c3bb7f
Tianze Yin <deyin12345@gmail.com>
2025-02-20 12:20:25 -0800

    Add axios dependency and update tasks route to fetch data from external API, and send the json data to the server

--- week6/routes/tasks.js
+++ week6/routes/tasks.js
@@ -1,10 +1,19 @@
 const express = require("express");
 const router = express.Router();
+const axios = require("axios");
 
+// this is the handler for the /tasks route
 router.get("/", function (req, res) {
-  res.send("<h1>List of All Tasks QQQpQ</h1>");
-  console.log(req.params);
-  console.log(req.query);
+  const promise = axios.get("https://jsonplaceholder.typicode.com/todos");
+  promise.then((response) => {
+    res.json(response.data);
+    console.log(response.data);
+  });
+  console.log(promise);
+
+  // res.send("<h1>List of All Tasks QQQpQ</h1>");
+  // console.log(req.params);
+  // console.log(req.query);
 });
 
 router.get("/:taskId", function (req, res) {
