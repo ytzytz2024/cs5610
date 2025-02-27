ec0a5efc98062507aa72c3d215d30207c67e777e
Tianze Yin <deyin12345@gmail.com>
2025-02-27 14:16:32 -0800

    Implement task form and redirect after task insertion

--- week6/routes/tasks.js
+++ week6/routes/tasks.js
@@ -8,11 +8,17 @@ router.post("/",async (req,res)=>{
   console.log(req.body);
   // call addToDB
   await db.addToDB(req.body);
-  res.send("Data received");
+  // res.send("Data received");
+  res.redirect("/tasks");
   } catch (error) {
     console.log(error.message);
   }
   
+});
+
+// show new form
+router.get('/newtask', function (req, res) {
+  res.render('taskForm');
 });
 
 // this is the handler for the /tasks route
