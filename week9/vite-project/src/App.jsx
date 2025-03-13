883c79430077310b2915b233505ca8b64a20af5b
Tianze Yin <deyin12345@gmail.com>
2025-03-13 12:36:40 -0700

    Add TasksList component and refactor App to display tasks with styling

--- week9/vite-project/src/App.jsx
+++ week9/vite-project/src/App.jsx
@@ -1,37 +1,14 @@
 import React from 'react'
 import Header from './components/Header'
+import TasksList from './components/TasksList'
 
 export default function App() {
   const appName = 'My Awesome App111'
-  const tasks = [
-    {
-      id: 1,
-      title: "Review week 9 material",
-      date: "June 4th at 1 pm",
-    },
-    {
-      id: 2,
-      title: "Do quiz 9",
-      date: "June 4th at 6 pm",
-    },
-    {
-      id: 3,
-      title: "Work on assignment 2",
-      date: "June 5th at 8 am",
-    },
-  ];
+
   return (
-    <div className = "appContainer">
+    <div className="appContainer">
       <Header myAppName={appName} version={2}/>
-      <ul>
-
-        {tasks.map((task) => (
-          <li key={task.id}>
-            {task.title}
-          </li>
-        ))}
-      </ul>
-
+      <TasksList />
     </div>
   ) 
 }
